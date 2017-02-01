9ad1d9fd20b1e87d711a0fa954ce7479607a7466
cryptology1996 <a.van.der.berg@st.hanze.nl>
2017-02-01 16:17:58 +0100

    Add files via upload

---
+++ src/Main/Main.java
@@ -0,0 +1,28 @@
+package Main;
+
+import javax.swing.*;
+
+import Controller.*;
+import View.*;
+
+
+public class Main
+{ 
+	private CarParkView carParkView;
+	
+	public static void main(String [] args)
+	{
+		JFrame frame = new JFrame();
+        Simulator s = new Simulator();
+		//controller=new Controller(model);
+	
+		frame.setSize(1280, 800);
+		frame.setResizable(false);
+		frame.setLayout(null);
+		//frame.getContentPane().add(CarParkView);
+		//frame.getContentPane().add(pieview);
+		//frame.getContentPane().add(Simulator);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}

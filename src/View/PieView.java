2b0fcf054747031b73c65d3effb511f2bf1a42b1
Bakkerino <k.m.bakker@st.hanze.nl>
2017-02-02 18:29:26 +0100

    PieView Fix

--- src/View/PieView.java
+++ src/View/PieView.java
@@ -16,20 +16,34 @@ public class PieView extends AbstractView {
     	super(simulator); 
     	setSize(300,300);
     }
+    
+    public int calculateDegrees(double aantalCars) 
+    {
+    	int degrees;
+    	double garageSize = 540;
+    	
+    	degrees = (int) (((100 / garageSize) * aantalCars) * 3.6);
+    	
+    	return degrees;
+    	
+    }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         aantalAdHoc = CarParkView.GetAdHoc();
         aantalPassCar = CarParkView.GetParkPass();
-        aantalEmpty = 500 - aantalPassCar - aantalAdHoc;
-        aantalReserved = aantalPassCar + aantalAdHoc;
+        aantalEmpty = 540 - aantalPassCar - aantalAdHoc;
+  
+        //System.out.println(aantalAdHoc + ": " + calculateDegrees(270));
         
         g.setColor(Color.red);
-        g.fillArc(10, 10, 250, 250, 0, aantalAdHoc);
+        g.fillArc(10, 10, 250, 250, 0, calculateDegrees(aantalAdHoc));
         g.setColor(Color.blue);
-        g.fillArc(10, 10, 250, 250, aantalAdHoc, aantalPassCar);
+        g.fillArc(10, 10, 250, 250, calculateDegrees(aantalAdHoc), 
+        							calculateDegrees(aantalPassCar));
         g.setColor(Color.yellow);
-        g.fillArc(10, 10, 250, 250, aantalReserved, aantalEmpty);
+        g.fillArc(10, 10, 250, 250, calculateDegrees(aantalAdHoc) + calculateDegrees(aantalPassCar), 
+        							calculateDegrees(aantalEmpty));
     }   
     
 }

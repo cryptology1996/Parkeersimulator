9fa453f0ce48c036339eac7dcf2483e44c6d62f0
Bakkerino <k.m.bakker@st.hanze.nl>
2017-02-02 19:22:31 +0100

    Percentage Legenda

--- src/View/TypeOfCarView.java
+++ src/View/TypeOfCarView.java
@@ -18,16 +18,19 @@ public class TypeOfCarView extends AbstractView{
 	private JTextField TreservationSpot;
 	private ParkeerGarage parkeerGarage;
 	
+	
 		
 	public TypeOfCarView(ParkeerGarage model) {
 		super(model);
     	this.parkeerGarage = model;
     	
+    	
+    	   	
 		this.setLayout(new GridLayout(0,1));
 		JPanel panel1 = new JPanel();
 		panel1.setBackground(Color.LIGHT_GRAY);
 		JLabel LadHoc = new JLabel("Precentage of adhoc car's :");
-		TadHoc = new JTextField(" " + 100);
+		TadHoc = new JTextField(" " + calculatePercentage(CarParkView.GetAdHoc()));
 		TadHoc.setEditable(false);
 		TadHoc.setBackground(Color.RED);
 		TadHoc.setBorder(BorderFactory.createLineBorder(Color.GRAY,1));
@@ -40,7 +43,7 @@ public class TypeOfCarView extends AbstractView{
 		panel2.setLayout(new FlowLayout());
 		panel2.setBackground(Color.LIGHT_GRAY);
 		JLabel LpassHolder = new JLabel("Precentage of parking pass car's :");
-		TpassHolder = new JTextField(" " + 100);
+		TpassHolder = new JTextField(" " + calculatePercentage(CarParkView.GetParkPass()));
 		TpassHolder.setEditable(false);
 		TpassHolder.setBackground(Color.BLUE);
 		TpassHolder.setBorder(BorderFactory.createLineBorder(Color.GRAY,1));
@@ -52,7 +55,7 @@ public class TypeOfCarView extends AbstractView{
 		JPanel panel3 = new JPanel();
 		panel3.setLayout(new FlowLayout());
 		panel3.setBackground(Color.LIGHT_GRAY);
-		JLabel Lreservation = new JLabel("Precentage of reservation car's :");
+		JLabel Lreservation = new JLabel("Precentage of empty spots :");
 		Treservation = new JTextField(" "+  100);
 		Treservation.setEditable(false);
 		Treservation.setBackground(Color.YELLOW);
@@ -74,5 +77,14 @@ public class TypeOfCarView extends AbstractView{
 		panel4.add(TreservationSpot);
 		this.add(panel4);
 		
+	}
+	
+	public double calculatePercentage(double aantalCars)
+	{
+		double percentage;
+		double garageSize = 540;
+		percentage = (((100 / garageSize) * aantalCars));
+		//System.out.println(percentage);
+		return percentage;
 	}
 }

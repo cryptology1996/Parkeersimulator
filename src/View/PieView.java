a9e869172f5b98ab6f00685a79b3b241d8b8ebae
Arneld van der Veen <ae.van.der.veen@st.hanze.nl>
2017-02-05 16:07:22 +0100

    again

--- src/View/PieView.java
+++ src/View/PieView.java
@@ -47,7 +47,7 @@ public class PieView extends AbstractView {
      */
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
+        
         aantalAdHoc = CarParkView.GetAdHoc();
         aantalPassCar = CarParkView.GetParkPass();
         aantalReservedSpot = CarParkView.GetReservedSpot();
